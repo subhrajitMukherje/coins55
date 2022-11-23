809f7c547953475dd4343e189204d43156bdc069
subhrajit07251 <subhrajitmukh1234@gmail.com>
2022-11-23 17:49:32 +0530

    cotinue

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,20 +1,35 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'b8ab1bdeb3msh7e210cfd959a132p1eddd0jsnd55cb032cfa4',
+};
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
-const cryptoApiHeaders ={
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': 'b8ab1bdeb3msh7e210cfd959a132p1eddd0jsnd55cb032cfa4'
-}
-const baseUrl ='https://coinranking1.p.rapidapi.com';
-const createRequest = (url) => ({url, header: cryptoApiHeaders})
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({baseUrl}),
-    endpoints:(builder) =>({
-        getCryptos: builder.query({
-            querry: ()=> createRequest ('/coins')
-        })
-    })
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+    }),
+
+    
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+  }),
 });
-export const{
-    useGetCryptosQuery,
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
 } = cryptoApi;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-key': 'b8ab1bdeb3msh7e210cfd959a132p1eddd0jsnd55cb032cfa4',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com'
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_NEWS_API_URL }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
